8de78a9f8cee6ecbea5b7af248572e94e7e5a06e
Onixaz <onixaz@gmail.com>
2018-12-31 16:07:35 +0200

    Adding comments to split_data.py

--- split_data.py
+++ split_data.py
@@ -1,11 +1,20 @@
 import numpy as np
 import os
 
+
+### lists where we will store our paths to images and labels ####
+
 image_paths = []
 label_paths = []
+
+#IMPORTANT! Specify how much of training data we want to split! ###
+# In case you want to work with smaller samples
+
+# I had 218 for example
 num_files = 218
 
 
+#### define progress bar ####
 def progress_bar(parsed, total):
     workdone = (parsed)/(total)
     print("\rProgress: [{0:50s}] {1:.1f}%".format(
@@ -13,14 +22,14 @@ def progress_bar(parsed, total):
 
 
 for i in range(num_files):
-
+    #########################################################
+    ### IMPORTANT!!! Change your data path accordingly!!! ###
+    #########################################################
     training_data = np.load(
         'E://CrossPlatform//Data//GTA5Training//training_data-{}.npy'.format(i+1))
 
     for k, sample in enumerate(training_data):
-        #print("this is : " + str(k))
-        #print("and this is" + str(sample[1]))
-
+        # set the path where you want your splti data to be stored
         img_file_dir = "data//images//"+str(i+1)
         if not os.path.exists(img_file_dir):
             os.makedirs(img_file_dir)
@@ -30,6 +39,7 @@ for i in range(num_files):
         np.save(img_file_path, sample[0])
         image_paths.append(img_file_path)
 
+        # same here
         lbl_file_dir = "data//labels//"+str(i+1)
         if not os.path.exists(lbl_file_dir):
             os.makedirs(lbl_file_dir)
@@ -41,10 +51,6 @@ for i in range(num_files):
 
     progress_bar(i, len(range(num_files)))
 
-
+### Save the data for later use ###
 np.save('data//image_paths.npy', image_paths)
 np.save('data//label_paths.npy', label_paths)
-
-
-#image_paths = np.load('data//image_paths.npy')
-# print(image_paths[800])
